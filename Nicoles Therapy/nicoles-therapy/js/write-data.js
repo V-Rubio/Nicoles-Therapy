bcae5766216074104bef6a280b183fb76c1b180a
V-Rubio <vi.rubioo@gmail.com>
2023-09-23 14:40:44 -0400

    Assignment 2

---
+++ Nicoles Therapy/nicoles-therapy/js/write-data.js
@@ -0,0 +1,10 @@
+//CREATING THE LISTENER - to add new object 
+$("#clearInput").click(function(){
+        $("#input1").val("");
+        $("#input2").val("");
+        $("#input3").val("");
+});
+
+$("#submitInput").click(function(){
+    alert("Submit Button was Pressed.");
+});
